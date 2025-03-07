2c06881c8e030a8f03b084f72abde215a236b0d4
Gusti Raditia Madya <gusti.madya@gdn-commerce.com>
2025-03-08 06:44:02 +0700

    console

--- api/ssr.js
+++ api/ssr.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = async (req, res) => {
+  console.log('Files in dist/client:', fs.readdirSync(path.join(__dirname, '..', 'dist', 'client')));
   try {
     // Build the absolute path to index.html based on __dirname
     const templatePath = path.join(__dirname, '..', 'dist', 'client', 'index.html');
@@ -20,6 +21,7 @@ module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(html);
   } catch (err) {
+    console.log('Files in dist/client:', fs.readdirSync(path.join(__dirname, '..', 'dist', 'client')));
     console.error('SSR error:', err);
     res.status(500).send('Internal Server Error');
   }
