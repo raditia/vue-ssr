e287b6d094b18e3389882123d16d5c4ab99a9f16
Gusti Raditia Madya <gusti.madya@gdn-commerce.com>
2025-03-08 06:53:34 +0700

    update dir

--- api/ssr.js
+++ api/ssr.js
@@ -4,14 +4,16 @@ const fs = require('fs');
 module.exports = async (req, res) => {
   try {
     // Build the absolute path to index.html based on __dirname
-    const templatePath = path.join(__dirname, '..', 'dist', 'client', 'index.html');
+    // const templatePath = path.join(__dirname, '..', 'dist', 'client', 'index.html');
+    const templatePath = path.join(process.cwd(), 'dist', 'client', 'index.html');
 
     // Optionally, log available files for debugging
     // console.log('Files in dist/client:', fs.readdirSync(path.join(__dirname, '..', 'dist', 'client')));
 
     const template = fs.readFileSync(templatePath, 'utf-8');
 
-    const entryServerPath = path.join(__dirname, '..', 'dist', 'server', 'entry-server.mjs');
+    // const entryServerPath = path.join(__dirname, '..', 'dist', 'server', 'entry-server.mjs');
+    const entryServerPath = path.join(process.cwd(), 'dist', 'server', 'entry-server.mjs');
     const { render } = await import(entryServerPath);
 
     const { appContent } = await render(req.url);
