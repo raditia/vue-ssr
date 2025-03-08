121d61eb660b2b97c5a469950e7f2ec9b6bd872b
Gusti Raditia Madya <gusti.madya@gdn-commerce.com>
2025-03-08 14:50:53 +0700

    add base again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/__vite_base__/',
+  base: './',
   plugins: [vue()],
   ssr: {},
   legacy: {
