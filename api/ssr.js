20e15b4b0e9b4d7ab4cf715893fb8aaadca133e4
Gusti Raditia Madya <gusti.madya@gdn-commerce.com>
2025-03-08 12:31:50 +0700

    revert

--- api/ssr.js
+++ api/ssr.js
@@ -3,25 +3,24 @@ const fs = require('fs');
 
 module.exports = async (req, res) => {
   try {
-  console.log('Files in dist/client:', fs.readdirSync(path.join(__dirname, '..', 'dist')));
     // Build the absolute path to index.html based on __dirname
     // const templatePath = path.join(__dirname, '..', 'dist', 'client', 'index.html');
-    // const templatePath = path.join(process.cwd(), 'dist', 'client', 'index.html');
-    //
-    // // Optionally, log available files for debugging
+    const templatePath = path.join(process.cwd(), 'dist', 'client', 'index.html');
+
+    // Optionally, log available files for debugging
     // console.log('Files in dist/client:', fs.readdirSync(path.join(__dirname, '..', 'dist', 'client')));
-    //
-    // const template = fs.readFileSync(templatePath, 'utf-8');
-    //
-    // // const entryServerPath = path.join(__dirname, '..', 'dist', 'server', 'entry-server.mjs');
-    // const entryServerPath = path.join(process.cwd(), 'dist', 'server', 'entry-server.mjs');
-    // const { render } = await import(entryServerPath);
-    //
-    // const { appContent } = await render(req.url);
-    // const html = template.replace(`<!--app-html-->`, appContent);
-    //
-    // res.setHeader('Content-Type', 'text/html');
-    // res.status(200).send(html);
+
+    const template = fs.readFileSync(templatePath, 'utf-8');
+
+    // const entryServerPath = path.join(__dirname, '..', 'dist', 'server', 'entry-server.mjs');
+    const entryServerPath = path.join(process.cwd(), 'dist', 'server', 'entry-server.mjs');
+    const { render } = await import(entryServerPath);
+
+    const { appContent } = await render(req.url);
+    const html = template.replace(`<!--app-html-->`, appContent);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(html);
   } catch (err) {
     console.error('SSR error:', err);
     res.status(500).send('Internal Server Error');
