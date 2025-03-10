0a9b252b3be052b49b6342434839a490a0dea652
Gusti Raditia Madya <gusti.madya@gdn-commerce.com>
2025-03-10 10:40:31 +0700

    add router index

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
 import { createSSRApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 
 export function createApp() {
   const app = createSSRApp(App)
